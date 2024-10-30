6d4541e00f590bd7592ea1f34fcb4baef4823719
Arib Husain <husainarib@gmail.com>
2024-10-30 17:49:54 -0400

    set up class activity 11 main.dart

--- activity10/lib/main.dart
+++ activity10/lib/main.dart
@@ -2,84 +2,165 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:..,
-    );
-  }
+const MyApp({Key? key}) : super(key: key);
+@override
+Widget build(BuildContext context) {
+return const MaterialApp(
+// Remove the debug banner
+debugShowCheckedModeBanner: false,
+title: 'Test',
+home: HomePage(),
+);
 }
-
-class Product {
-  String id;
-  String name;
-  double price;
-
-  Product({
-    required this.id,
-    required this.name,
-    required this.price,
-  });
-
-  factory Product.fromMap(Map<String, dynamic> map) {
-    return Product(
-      id: map['id'] as String,
-      name: map['name'] as String,
-      price: map['price'] as double,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'price': price,
-    };
-  }
 }
-
-class ProductService { 
-  final CollectionReference productsCollection = FirebaseFirestore.instance.collection('products');
-
-  // Method to create a new product
-  Future<void> createProduct (Product product) { 
-    return productsCollection.doc(product.id).set(product.toMap());
-  }
-
-   // Method to read all products
-  Future<List<Product>> getProducts() {
-    return productsCollection.get().then((snapshot) {
-      return snapshot.docs.map((doc) {
-        return Product.fromMap(doc.data() as Map<String, dynamic>);
-      }).toList();
-    });
-  }
-
-  // Method to update a product
-  Future<void> updateProduct(Product product) {
-    return productsCollection.doc(product.id).update(product.toMap());
-  }
-
-  // Method to delete a product
-  Future<void> deleteProduct(String productId) {
-    return productsCollection.doc(productId).delete();
-  }
+class HomePage extends StatefulWidget {
+const HomePage({Key? key}) : super(key: key);
+@override
+State<HomePage> createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+// text fields' controllers
+final TextEditingController _nameController = TextEditingController();
+final TextEditingController _priceController = TextEditingController();
+final CollectionReference _products =
+FirebaseFirestore.instance.collection('products');
+Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async
+{
+String action = 'create';
+if (documentSnapshot != null) {
+action = 'update';
+_nameController.text = documentSnapshot['name'];
+_priceController.text = documentSnapshot['price'].toString();
+}
+await showModalBottomSheet(
+isScrollControlled: true,
+context: context,
+builder: (BuildContext ctx) {
+return Padding(
+padding: EdgeInsets.only(
+top: 20,
+left: 20,
+right: 20,
+bottom: MediaQuery.of(ctx).viewInsets.bottom + 20,
+),
+child: Column(
+mainAxisSize: MainAxisSize.min,
+crossAxisAlignment: CrossAxisAlignment.start,
+children: [
+TextField(
+controller: _nameController,
+decoration: const InputDecoration(labelText: 'Name'),
+),
+TextField(
+keyboardType: TextInputType.numberWithOptions(decimal:
+true),
+controller: _priceController,
+decoration: const InputDecoration(
+labelText: 'Price',
+),
+),
+const SizedBox(
+height: 20,
+),
+ElevatedButton(
+child: Text(action == 'create' ? 'Create' : 'Update'),
+onPressed: () async {
+String name = _nameController.text;
+double price = double.parse(_priceController.text);
+if (name.isNotEmpty && price != null) {
+if (action == 'create') {
+// Persist a new product to Firestore
+await _products.add({"name": name, "price": price});
+}
+if (action == 'update') {
+// Update the product
+await _products.doc(documentSnapshot!.id).update({
+"name": name,
+"price": price,
+});
+}
+_nameController.text = '';
+_priceController.text = '';
+Navigator.of(context).pop();
+}
+},
+)
+],
+),
+);
+},
+);
+}
+// Deleting a product by id
+Future<void> _deleteProduct(String productId) async {
+ScaffoldMessenger.of(context).showSnackBar(
+const SnackBar(
+content: Text('You have successfully deleted a product'),
+),
+);
+}
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: const Text('CRUD operations'),
+),
+// Using StreamBuilder to display all products from Firestore in
+real-time
+body: StreamBuilder(
+stream: _products.snapshots(),
+builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+if (streamSnapshot.hasData) {
+return ListView.builder(
+itemCount: streamSnapshot.data!.docs.length,
+itemBuilder: (context, index) {
+final DocumentSnapshot documentSnapshot =
+streamSnapshot.data!.docs[index];
+return Card(
+margin: const EdgeInsets.all(10),
+child: ListTile(
+title: Text(documentSnapshot['name']),
+subtitle: Text(documentSnapshot['price'].toString()),
+trailing: SizedBox(
+width: 100,
+child: Row(
+children: [
+IconButton(
+icon: const Icon(Icons.edit),
+onPressed: () =>
+_createOrUpdate(documentSnapshot),
+),
+IconButton(
+icon: const Icon(Icons.delete),
+onPressed: () =>
+_deleteProduct(documentSnapshot.id),
+),
+],
+),
+),
+),
+);
+},
+);
+}
+return const Center(
+child: CircularProgressIndicator(),
+);
+},
+),
+// Add new product
+floatingActionButton: FloatingActionButton(
+onPressed: () => _createOrUpdate(),
+child: const Icon(Icons.add),
+),
+);
+}
 }
