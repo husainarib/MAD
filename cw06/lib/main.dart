5afe617024a689ae2901160c0b3461867d9e6201
Arib Husain <husainarib@gmail.com>
2024-11-07 12:27:26 -0500

    Create a Nested list that will include a sub-list

--- cw06/lib/main.dart
+++ cw06/lib/main.dart
@@ -17,6 +17,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Task {
+  String name;
+  bool isComplete;
+  Map<String, List<Map<String, dynamic>>> nestedTasks;
+
+  Task({
+    required this.name,
+    this.isComplete = false,
+    required this.nestedTasks,
+  });
+}
+
 class TaskListScreen extends StatefulWidget {
   @override
   _TaskListScreenState createState() => _TaskListScreenState();
@@ -24,43 +36,49 @@ class TaskListScreen extends StatefulWidget {
 
 class _TaskListScreenState extends State<TaskListScreen> {
   final TextEditingController _taskController = TextEditingController();
-  List<String> _tasks = [];
-  Map<String, List<Map<String, dynamic>>> _nestedTasks = {
-    'Monday': [
-      {
-        'time': '9 am - 10 am',
-        'tasks': ['HW1', 'Essay2']
-      },
-      {
-        'time': '12 pm - 2 pm',
-        'tasks': ['Project A', 'Read Chapter 5']
-      },
-    ],
-    'Tuesday': [
-      {
-        'time': '10 am - 11 am',
-        'tasks': ['Meeting', 'Exercise']
-      },
-      {
-        'time': '3 pm - 4 pm',
-        'tasks': ['Research', 'Notes']
-      },
-    ],
-    // Add more days and tasks as needed
-  };
+  final TextEditingController _subTaskController = TextEditingController();
+  List<Task> _tasks = [];
 
-  void _add() {
+  void _addTask() {
     if (_taskController.text.isNotEmpty) {
       setState(() {
-        _tasks.add(_taskController.text);
+        _tasks.add(Task(
+          name: _taskController.text,
+          nestedTasks: {},
+        ));
       });
       _taskController.clear();
     }
   }
 
-  void _delete(int index) {
+  void _deleteTask(int index) {
     setState(() {
       _tasks.removeAt(index);
+    });
+  }
+
+  void _addSubTask(Task task, String day, String time) {
+    setState(() {
+      if (!task.nestedTasks.containsKey(day)) {
+        task.nestedTasks[day] = [];
+      }
+      task.nestedTasks[day]!.add({
+        'time': time,
+        'tasks': [_subTaskController.text],
+      });
+      _subTaskController.clear();
+    });
+  }
+
+  void _deleteSubTask(Task task, String day, int timeIndex, String subTask) {
+    setState(() {
+      task.nestedTasks[day]![timeIndex]['tasks'].remove(subTask);
+      if (task.nestedTasks[day]![timeIndex]['tasks'].isEmpty) {
+        task.nestedTasks[day]!.removeAt(timeIndex);
+      }
+      if (task.nestedTasks[day]!.isEmpty) {
+        task.nestedTasks.remove(day);
+      }
     });
   }
 
@@ -71,89 +89,208 @@ class _TaskListScreenState extends State<TaskListScreen> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
                 Expanded(
                   child: TextField(
                     controller: _taskController,
                     decoration: InputDecoration(
-                      labelText: 'Enter Task Name',
+                      labelText: 'Enter task name',
                     ),
                   ),
                 ),
                 IconButton(
                   icon: Icon(Icons.add),
-                  onPressed: _add,
+                  onPressed: _addTask,
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ListView(
-              children: [
-                ..._tasks.asMap().entries.map((entry) {
-                  int index = entry.key;
-                  String task = entry.value;
-                  return ListTile(
-                    title: Text(task),
-                    leading: Checkbox(
-                      value: false,
-                      onChanged: (value) {
-                        setState(() {
-                          // TODO Mark task as complete or incomplete
-                        });
-                      },
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => _delete(index),
-                    ),
-                  );
-                }).toList(),
-                ..._nestedTasks.keys.map((day) {
-                  return ExpansionTile(
-                    title: Text(day,
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    children: _nestedTasks[day]!.map((schedule) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(schedule['time'],
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w600)),
-                            ...schedule['tasks'].map<Widget>((subTask) {
-                              return ListTile(
-                                title: Text(subTask),
-                                leading: Checkbox(
-                                  value: false,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      // TODO Handle task completion
-                                    });
-                                  },
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return ExpansionTile(
+                  title: Row(
+                    children: [
+                      Checkbox(
+                        value: task.isComplete,
+                        onChanged: (value) {
+                          setState(() {
+                            task.isComplete = value!;
+                          });
+                        },
+                      ),
+                      Expanded(child: Text(task.name)),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteTask(index),
+                      ),
+                    ],
+                  ),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: _subTaskController,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter sub-task name',
+                                  ),
                                 ),
-                                trailing: IconButton(
-                                  icon: Icon(Icons.delete),
-                                  onPressed: () {
-                                    setState(() {
-                                      schedule['tasks'].remove(subTask);
-                                    });
-                                  },
-                                ),
-                              );
-                            }).toList(),
-                          ],
-                        ),
-                      );
-                    }).toList(),
-                  );
-                }).toList(),
-              ],
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.add),
+                                onPressed: () {
+                                  _showAddSubTaskDialog(task);
+                                },
+                              ),
+                            ],
+                          ),
+                          if (task.nestedTasks.isNotEmpty)
+                            for (var day in task.nestedTasks.keys)
+                              ExpansionTile(
+                                title: Text(day,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold)),
+                                children: [
+                                  for (var timeSlotIndex = 0;
+                                      timeSlotIndex <
+                                          task.nestedTasks[day]!.length;
+                                      timeSlotIndex++)
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 4.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                              task.nestedTasks[day]![
+                                                  timeSlotIndex]['time'],
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w600)),
+                                          ...task
+                                              .nestedTasks[day]![timeSlotIndex]
+                                                  ['tasks']
+                                              .map<Widget>((subTask) {
+                                            return ListTile(
+                                              title: Text(subTask),
+                                              leading: Checkbox(
+                                                value: false,
+                                                onChanged: (value) {
+                                                  setState(() {
+                                                    // TODO Mark sub-task as complete
+                                                  });
+                                                },
+                                              ),
+                                              trailing: IconButton(
+                                                icon: Icon(Icons.delete),
+                                                onPressed: () {
+                                                  _deleteSubTask(task, day,
+                                                      timeSlotIndex, subTask);
+                                                },
+                                              ),
+                                            );
+                                          }).toList(),
+                                        ],
+                                      ),
+                                    ),
+                                ],
+                              ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showAddSubTaskDialog(Task task) {
+    String selectedDay = 'Monday';
+    String selectedTime = '9 am - 10 am';
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add Sub-Task'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _subTaskController,
+              decoration: InputDecoration(labelText: 'Sub-task name'),
+            ),
+            DropdownButton<String>(
+              value: selectedDay,
+              onChanged: (newDay) {
+                setState(() {
+                  selectedDay = newDay!;
+                });
+              },
+              items: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+                  .map((day) => DropdownMenuItem(
+                        value: day,
+                        child: Text(day),
+                      ))
+                  .toList(),
+            ),
+            DropdownButton<String>(
+              value: selectedTime,
+              onChanged: (newTime) {
+                setState(() {
+                  selectedTime = newTime!;
+                });
+              },
+              items: [
+                '9 am - 10 am',
+                '11 am - 12 pm',
+                '12 pm - 1 pm',
+                '1 pm - 2 pm',
+                '2 pm - 3 pm',
+                '3 pm - 4 pm',
+                '4 pm - 5 pm',
+                '5 pm - 6 pm',
+                '6 pm - 7 pm',
+                '7 pm - 8 pm',
+                '8 pm - 9 pm',
+                '9 pm - 10 pm',
+                '10 pm - 11 pm',
+              ]
+                  .map((time) => DropdownMenuItem(
+                        value: time,
+                        child: Text(time),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              _addSubTask(task, selectedDay, selectedTime);
+              Navigator.pop(context);
+            },
+            child: Text('Add'),
           ),
         ],
       ),
