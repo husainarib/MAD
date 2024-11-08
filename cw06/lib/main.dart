00bda72f878168988143206c840b9e703702ce14
Arib Husain <husainarib@gmail.com>
2024-11-07 20:42:16 -0500

    fix firebase connection and UI design

--- cw06/lib/main.dart
+++ cw06/lib/main.dart
@@ -5,238 +5,131 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Task Manager',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TaskListScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class Task {
-  String id;
-  String name;
-  bool isComplete;
-  Map<String, List<Map<String, dynamic>>> nestedTasks;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  Task({
-    required this.id,
-    required this.name,
-    this.isComplete = false,
-    required this.nestedTasks,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'isComplete': isComplete,
-      'nestedTasks': nestedTasks,
-    };
-  }
-
-  static Task fromMap(Map<String, dynamic> map, String id) {
-    return Task(
-      id: id,
-      name: map['name'],
-      isComplete: map['isComplete'],
-      nestedTasks: Map<String, List<Map<String, dynamic>>>.from(map['nestedTasks']),
-    );
-  }
-}
-
-class TaskListScreen extends StatefulWidget {
   @override
-  _TaskListScreenState createState() => _TaskListScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _TaskListScreenState extends State<TaskListScreen> {
+class _HomePageState extends State<HomePage> {
   final TextEditingController _taskController = TextEditingController();
-  final TextEditingController _subTaskController = TextEditingController();
+  final CollectionReference _tasksCollection =
+      FirebaseFirestore.instance.collection('tasks');
 
-  void _addTask() {
-    if (_taskController.text.isNotEmpty) {
-      final task = Task(
-        id: FirebaseFirestore.instance.collection('tasks').doc().id,
-        name: _taskController.text,
-        nestedTasks: {},
-      );
-      FirebaseFirestore.instance.collection('tasks').doc(task.id).set(task.toMap());
+// // TESTING FIREBASE CONNECTION
+//   Future<void> testWrite() async {
+//     try {
+//       await FirebaseFirestore.instance.collection('testCollection').add({
+//         'testField': 'testValue',
+//       });
+//       print('Write successful');
+//     } catch (e) {
+//       print('Error writing to Firestore: $e');
+//     }
+//   }
+
+  Future<void> _addTask(String taskName) async {
+    if (taskName.isNotEmpty) {
+      await _tasksCollection.add({
+        'name': taskName,
+        'isCompleted': false,
+      });
       _taskController.clear();
     }
   }
 
-  void _toggleTaskCompletion(Task task) {
-    FirebaseFirestore.instance.collection('tasks').doc(task.id).update({
-      'isComplete': !task.isComplete,
+  Future<void> _toggleTaskCompletion(DocumentSnapshot taskDoc) async {
+    bool currentStatus = taskDoc['isCompleted'];
+    await _tasksCollection.doc(taskDoc.id).update({
+      'isCompleted': !currentStatus,
     });
   }
 
-  void _deleteTask(String taskId) {
-    FirebaseFirestore.instance.collection('tasks').doc(taskId).delete();
+  Future<void> _deleteTask(String taskId) async {
+    await _tasksCollection.doc(taskId).delete();
   }
 
-  void _addSubTask(Task task, String day, String time) {
-    setState(() {
-      if (!task.nestedTasks.containsKey(day)) {
-        task.nestedTasks[day] = [];
-      }
-      task.nestedTasks[day]!.add({
-        'time': time,
-        'tasks': [_subTaskController.text],
-      });
-      FirebaseFirestore.instance
-          .collection('tasks')
-          .doc(task.id)
-          .update({'nestedTasks': task.nestedTasks});
-      _subTaskController.clear();
-    });
+  Future<void> _addSubTask(
+      String taskId, String subTaskName, String timeFrame) async {
+    if (subTaskName.isNotEmpty && timeFrame.isNotEmpty) {
+      await _tasksCollection.doc(taskId).collection('subTasks').add(
+          {'name': subTaskName, 'timeFrame': timeFrame, 'isCompleted': false});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Task Manager')),
+      appBar: AppBar(
+        title: const Text('Task Manager'),
+      ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
+                // // TESTING FIREBASE CONNECTION
+                // ElevatedButton(
+                //   onPressed: testWrite,
+                //   child: const Text('Test Firestore Write'),
+                // ),
                 Expanded(
                   child: TextField(
                     controller: _taskController,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       labelText: 'Enter task name',
                     ),
                   ),
                 ),
                 IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: _addTask,
+                  icon: const Icon(Icons.add),
+                  onPressed: () => _addTask(_taskController.text),
                 ),
               ],
             ),
           ),
           Expanded(
             child: StreamBuilder(
-              stream: FirebaseFirestore.instance.collection('tasks').snapshots(),
+              stream: _tasksCollection.snapshots(),
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) return CircularProgressIndicator();
-                final tasks = snapshot.data!.docs.map((doc) {
-                  return Task.fromMap(doc.data() as Map<String, dynamic>, doc.id);
-                }).toList();
-
-                return ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    final task = tasks[index];
-                    return ExpansionTile(
-                      title: Row(
-                        children: [
-                          Checkbox(
-                            value: task.isComplete,
-                            onChanged: (value) => _toggleTaskCompletion(task),
-                          ),
-                          Expanded(child: Text(task.name)),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () => _deleteTask(task.id),
-                          ),
-                        ],
-                      ),
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: TextField(
-                                      controller: _subTaskController,
-                                      decoration: InputDecoration(
-                                        labelText: 'Enter sub-task name',
-                                      ),
-                                    ),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.add),
-                                    onPressed: () {
-                                      _showAddSubTaskDialog(task);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              if (task.nestedTasks.isNotEmpty)
-                                for (var day in task.nestedTasks.keys)
-                                  ExpansionTile(
-                                    title: Text(day,
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold)),
-                                    children: [
-                                      for (var timeSlotIndex = 0;
-                                          timeSlotIndex <
-                                              task.nestedTasks[day]!.length;
-                                          timeSlotIndex++)
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              vertical: 4.0),
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                  task.nestedTasks[day]![
-                                                      timeSlotIndex]['time'],
-                                                  style: TextStyle(
-                                                      fontSize: 14,
-                                                      fontWeight: FontWeight.w600)),
-                                              ...task
-                                                  .nestedTasks[day]![timeSlotIndex]
-                                                      ['tasks']
-                                                  .map<Widget>((subTask) {
-                                                return ListTile(
-                                                  title: Text(subTask),
-                                                  leading: Checkbox(
-                                                    value: false,
-                                                    onChanged: (value) {
-                                                      setState(() {
-                                                        // Mark sub-task as complete
-                                                      });
-                                                    },
-                                                  ),
-                                                  trailing: IconButton(
-                                                    icon: Icon(Icons.delete),
-                                                    onPressed: () {
-                                                      _deleteSubTask(task, day,
-                                                          timeSlotIndex, subTask);
-                                                    },
-                                                  ),
-                                                );
-                                              }).toList(),
-                                            ],
-                                          ),
-                                        ),
-                                    ],
-                                  ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final DocumentSnapshot taskDoc =
+                          snapshot.data!.docs[index];
+                      return TaskTile(
+                        taskDoc: taskDoc,
+                        onToggleComplete: () => _toggleTaskCompletion(taskDoc),
+                        onDelete: () => _deleteTask(taskDoc.id),
+                        onAddSubTask: (subTaskName, timeFrame) =>
+                            _addSubTask(taskDoc.id, subTaskName, timeFrame),
+                      );
+                    },
+                  );
+                }
+                return const Center(child: CircularProgressIndicator());
               },
             ),
           ),
@@ -244,88 +137,117 @@ class _TaskListScreenState extends State<TaskListScreen> {
       ),
     );
   }
+}
 
-  void _showAddSubTaskDialog(Task task) {
-    String selectedDay = 'Monday';
-    String selectedTime = '9 am - 10 am';
+class TaskTile extends StatefulWidget {
+  final DocumentSnapshot taskDoc;
+  final VoidCallback onToggleComplete;
+  final VoidCallback onDelete;
+  final Function(String subTaskName, String timeFrame) onAddSubTask;
 
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Add Sub-Task'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
+  const TaskTile({
+    Key? key,
+    required this.taskDoc,
+    required this.onToggleComplete,
+    required this.onDelete,
+    required this.onAddSubTask,
+  }) : super(key: key);
+
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  final TextEditingController _subTaskNameController = TextEditingController();
+  final TextEditingController _timeFrameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: ExpansionTile(
+        title: Row(
           children: [
-            TextField(
-              controller: _subTaskController,
-              decoration: InputDecoration(labelText: 'Sub-task name'),
+            Checkbox(
+              value: widget.taskDoc['isCompleted'],
+              onChanged: (value) => widget.onToggleComplete(),
             ),
-            DropdownButton<String>(
-              value: selectedDay,
-              onChanged: (newDay) {
-                setState(() {
-                  selectedDay = newDay!;
-                });
-              },
-              items: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-                  .map((day) => DropdownMenuItem(
-                        value: day,
-                        child: Text(day),
-                      ))
-                  .toList(),
+            Text(
+              widget.taskDoc['name'],
+              style: TextStyle(
+                decoration: widget.taskDoc['isCompleted']
+                    ? TextDecoration.lineThrough
+                    : null,
+              ),
             ),
-            DropdownButton<String>(
-              value: selectedTime,
-              onChanged: (newTime) {
-                setState(() {
-                  selectedTime = newTime!;
-                });
-              },
-              items: [
-                '9 am - 10 am',
-                '12 pm - 2 pm',
-                '3 pm - 4 pm',
-              ]
-                  .map((time) => DropdownMenuItem(
-                        value: time,
-                        child: Text(time),
-                      ))
-                  .toList(),
+            const Spacer(),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: widget.onDelete,
             ),
           ],
         ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
+        children: [
+          StreamBuilder(
+            stream: widget.taskDoc.reference.collection('subTasks').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: snapshot.data!.docs.map((subTaskDoc) {
+                    return ListTile(
+                      title: Text(
+                          "${subTaskDoc['timeFrame']}: ${subTaskDoc['name']}"),
+                      leading: Checkbox(
+                        value: subTaskDoc['isCompleted'],
+                        onChanged: (value) {
+                          subTaskDoc.reference
+                              .update({'isCompleted': value ?? false});
+                        },
+                      ),
+                    );
+                  }).toList(),
+                );
+              }
+              return const Center(child: CircularProgressIndicator());
             },
-            child: Text('Cancel'),
           ),
-          TextButton(
-            onPressed: () {
-              _addSubTask(task, selectedDay, selectedTime);
-              Navigator.pop(context);
-            },
-            child: Text('Add'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _subTaskNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Sub-task name',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    controller: _timeFrameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Time frame',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    widget.onAddSubTask(
+                      _subTaskNameController.text,
+                      _timeFrameController.text,
+                    );
+                    _subTaskNameController.clear();
+                    _timeFrameController.clear();
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
-  }
-
-  void _deleteSubTask(Task task, String day, int timeIndex, String subTask) {
-    setState(() {
-      task.nestedTasks[day]![timeIndex]['tasks'].remove(subTask);
-      if (task.nestedTasks[day]![timeIndex]['tasks'].isEmpty) {
-        task.nestedTasks[day]!.removeAt(timeIndex);
-      }
-      if (task.nestedTasks[day]!.isEmpty) {
-        task.nestedTasks.remove(day);
-      }
-      FirebaseFirestore.instance
-          .collection('tasks')
-          .doc(task.id)
-          .update({'nestedTasks': task.nestedTasks});
-    });
   }
 }
